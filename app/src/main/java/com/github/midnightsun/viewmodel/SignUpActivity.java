edb5ed00a7c28dc70917ffdeefa5b8cc346c7051
jasonCheng40 <chengjason402@gmail.com>
2019-01-06 22:25:10 +0800

    [add] add sign up page

--- app/src/main/java/com/github/midnightsun/viewmodel/SignUpActivity.java
+++ app/src/main/java/com/github/midnightsun/viewmodel/SignUpActivity.java
@@ -1,15 +1,24 @@
-package com.github.midnightsun;
+package com.github.midnightsun.viewmodel;
 
-import android.app.Application;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.github.midnightsun.datatype.UserData;
+import com.github.midnightsun.service.MyApplication;
+import com.github.midnightsun.R;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +29,8 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText psd_input;
     private Button completed_btn;
     private Button cancled_btn;
+    private UserData data_receive;
+    private static final String uri = "/midnightapisvr/api/user/userregister";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,34 +52,7 @@ public class SignUpActivity extends AppCompatActivity {
                 String nickname = nick_name_input.getText().toString();
                 String username = user_name_input.getText().toString();
                 String psd = psd_input.getText().toString();
-                Toast toast;
-
-                //post data to remote host
-                postSignUpDataToHost(nickname, username, psd);
-
-                if (!isValid()) {
-                    toast =  Toast.makeText(SignUpActivity.this,
-                            "用户名或密码存在格式错误",Toast.LENGTH_SHORT);
-                    toast.show();
-                    user_name_input.setText("");
-                    psd_input.setText("");
-                }
-                else {
-                    toast =  Toast.makeText(SignUpActivity.this,
-                            "注册成功",Toast.LENGTH_SHORT);
-                    toast.show();
-                    // save user infor
-                    SharedPreferences sharedPreferences =
-                            getSharedPreferences(username, Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("psd", psd);
-                    editor.putString("nickname",nickname);
-                    editor.commit();
-
-                    // switch to login activity
-                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
+                signUp(nickname, username, psd, "sign-up");
             }
         });
     }
@@ -77,18 +61,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         //通过Tag标签取消请求队列中对应的全部请求
-        MyApplication.getHttpQueues().cancelAll("register");
-    }
-
-    private void postSignUpDataToHost(String nickname, String username, String psd) {
-        Map<String, String> map = new HashMap<>();
-        map.put("nickname", nickname);
-        map.put("username", username);
-        map.put("password", psd);
-
-        MyApplication.volleyPost(SignUpActivity.this,
-                "/midnightapisvr/api/user/userregister",
-                "register", map);
+        MyApplication.getHttpQueues().cancelAll("sign-up");
     }
 
     private void setCancledButtonClickListener() {
@@ -106,7 +79,50 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-    Boolean isValid() {
-        return true;
+    private void signUp(String nickname, String username, String psd, String tag) {
+        Map<String, String> map = new HashMap<>();
+        map.put("nickname", nickname);
+        map.put("username", username);
+        map.put("password", psd);
+
+        JSONObject object = new JSONObject(map);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                MyApplication.host + uri, object,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject jsonObject) {
+                        Log.i("sign-up", jsonObject.toString());
+                        Gson gson = new Gson();
+                        data_receive = gson.fromJson(jsonObject.toString(), UserData.class);
+                        UIfresh();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.i("sign-up", volleyError.toString());
+                    }
+                });
+
+        request.setTag(tag);
+        MyApplication.getHttpQueues().add(request);
+    }
+
+    void UIfresh() {
+        if (data_receive.rtn != 0) {
+            Toast.makeText(SignUpActivity.this,
+                    data_receive.msg, Toast.LENGTH_SHORT).show();
+            user_name_input.setText("");
+            psd_input.setText("");
+        }
+        else {
+            Toast.makeText(SignUpActivity.this,
+                    "注册成功",Toast.LENGTH_SHORT).show();
+
+            // switch to login activity
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
